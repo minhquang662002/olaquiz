6eb6b9e991ca102ac1620d4ffe11508c13e3430c
Minh Quang <minhquang662002@gmail>
2023-01-22 00:45:08 +0700

    test

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,43 +1,47 @@
-import { prisma } from '../../../utils/db';
-import bcrypt from 'bcrypt'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { User } from '@prisma/client';
+import { prisma } from "../../../utils/db";
+import bcrypt from "bcrypt";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { User } from "@prisma/client";
 
 // const SECRET = process.env.SECRET
-
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<User | string>
 ) {
   try {
-    
-  if(!req.body){
-    return res.status(400).json("No content!")
-  }
-  const {firstName, lastName, email, password, confirm_password} = req.body
-  if(!firstName.trim() || !lastName.trim() || !email || !password || !confirm_password){
-    return res.status(400).json("Missing field!")
-  }
-  if(password !== confirm_password){
-    return res.status(400).json("Password is not match!")
-  }
-  const isExisted = await prisma.user.findFirst({where: {email}})
-  if(isExisted){
-    return res.status(400).json("This email has already existed!")
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await prisma.user.create({data: {
-    firstName,
-    lastName,
-    email,
-    password: hashedPassword,
-}})
+    if (!req.body) {
+      return res.status(400).json("No content!");
+    }
+    const { firstName, lastName, email, password, confirm_password } = req.body;
+    if (
+      !firstName.trim() ||
+      !lastName.trim() ||
+      !email ||
+      !password ||
+      !confirm_password
+    ) {
+      return res.status(400).json("Missing field!");
+    }
+    if (password !== confirm_password) {
+      return res.status(400).json("Password is not match!");
+    }
+    const isExisted = await prisma.user.findFirst({ where: { email } });
+    if (isExisted) {
+      return res.status(400).json("This email has already existed!");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-
-  return res.status(200).json(newUser)
+    return res.status(200).json(newUser);
   } catch (error) {
-    return res.status(500).json("Internal server error!")
+    return res.status(500).json(JSON.stringify(error));
   }
 }
